f468f8b921ac153c3b4319490e0b5ea632756697
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-11-28 09:59:09 -0700

    Update Alfred Quotes

---
+++ AlfredQuotes.java
@@ -0,0 +1,23 @@
+import java.util.Date;
+public class AlfredQuotes {
+
+    public String basicGreeting() {
+
+        return " Hello, lovely to see you. How are you?";
+    }
+
+    public String guestGreeting(String name) {
+
+        return "place holder for guest greeting return string";
+    }
+
+    public String dateannouncement() {
+
+        return "place holder for date announcement return string";
+    }
+
+    public String respondBeforeAlexis(String conversation) {
+
+        return "for snarky response return string";
+    }
+}
