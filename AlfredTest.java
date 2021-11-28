94c88b7de0b92b3ae26a20a6fc6fbd9a056633fa
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-11-28 10:07:50 -0700

    Update Alfred Test

---
+++ AlfredTest.java
@@ -0,0 +1,17 @@
+public class AlfredTest{
+
+    public static void main(String[] args) {
+
+        AlfredQoutes alfredBot = new AlfredQuotes();
+
+        String testGreeting = alfredBot.basicGreeting();
+        String testGuestGreeting = alfredBot.guestGreeting("Beth Kane");
+        String testDateAnnouncement = alfredBot.dateannouncement();
+
+        String alexisTest = alfredBot.respondBeforeAlexis("Alexis! Play some low-fi beats.");
+        String AlfredTest = alfredBot.respondBeforeAlexis("I can't find my yo-yo. Maybe Alfred will know where it is.");
+        String notRelevantTest = alfredBot.respondBeforeAlexis("Maybe that's what Batman is about. Not winning. But failing..");
+
+        System.out.println(testGreeting);
+    }
+}
